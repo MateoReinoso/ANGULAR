1d310b03f8ecd3ce1d33506cf7b8e46b00655796
MateoReinoso <mateo_riva@hotmail.com>
2021-09-23 23:48:40 -0500

    Creacion primer proyecto

---
+++ PrimerPrograma/mi-primera-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',//plantilla que se va a usar 
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //atributos
+  title = 'mi-primera-app';
+  name = 'hola';
+}
